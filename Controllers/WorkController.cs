31c075318b44b4a1cc6c58086fa35afa45538bd5
muradmecidov <muradmecidov1499@gmail.com>
2023-05-14 23:56:08 +0400

    TAsk6

---
+++ Controllers/WorkController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebFrontToBack.Controllers
+{
+    public class WorkController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
